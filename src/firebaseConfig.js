9c2aec490198ca6c6cd04946104f48f07573de02
JoseAngelHdezHdez <joseangel280298@gmail.com>
2023-03-03 12:19:49 -0600

    auth casi casi

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBh_hUX8EBd2bEljXchyEnIb7pt0GNJW18",
+  authDomain: "vue-3-course-d4d8a.firebaseapp.com",
+  projectId: "vue-3-course-d4d8a",
+  storageBucket: "vue-3-course-d4d8a.appspot.com",
+  messagingSenderId: "765773202828",
+  appId: "1:765773202828:web:7c47b044099cbd0d9782a9"
+};
+
+initializeApp(firebaseConfig);
+const auth = getAuth()
+
+export {auth}; 
